1231852747a6eccc8e9c283d35185436887f0f58
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-16 09:30:14 +0700

    types

---
+++ a.js
@@ -0,0 +1,7 @@
+let str = 'Hello world'; // var ES6
+str = 100;
+const str2 = 'React Native Khoa Pham';
+console.log(str);
+
+// 1. Nguyen thuy: number, string, boolean, null
+// 2. Kieu hon hop: Object, Function, Array
