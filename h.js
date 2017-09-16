be9a41821a453a5dd056921f8041abcb38b8527b
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-16 14:06:41 +0700

    fn

--- h.js
+++ h.js
@@ -22,10 +22,16 @@ function inSo(fn) {
     }
 }
 
-inSo(function(i) {
-    return i % 2 === 0;
+//arrow function
+
+inSo(i => {
+    return i % 2 === 0
 });
 
+// inSo(i => Math.sqrt(i) % 1 === 0);
 
+// inSo(i => i % 3 === 1);
 
-// Math.sqrt()
+// // Math.sqrt()
+// const doubleX = x => 2 * x; // function(x) { return 2 * x }
+// console.log(doubleX(100));
